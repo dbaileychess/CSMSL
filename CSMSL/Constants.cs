4ef6578558a2e9df24fee31eb3295ed045dbf1b5
Derek Bailey <dbaileychess@gmail.com>
2012-07-18 16:44:52 -0500

    Initial Commit to C# Mass Spectrometry Library

---
+++ CSMSL/Constants.cs
@@ -0,0 +1,22 @@
+﻿namespace CSMSL
+{
+    /// <summary>
+    /// A collection of immutable constants and physical properties.
+    /// Sources include:
+    /// http://physics.nist.gov/cuu/Constants/index.html
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// The mass of the subatmoic particle with a single negative elementary charge in
+        /// atomic units (u)
+        /// </summary>
+        public const double ELECTRON = 0.00054857990946;
+
+        /// <summary>
+        /// The mass of the subatomic particle with a single elementary charge in atomic
+        /// units (u)
+        /// </summary>
+        public const double PROTON = 1.007276466812;
+    }
+}
---
+++ CSMSL/Mass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CSMSL
+{
+    public class Mass
+    {
+        #region Static Methods
+
+        /// <summary>
+        /// Calculates the mass of a given m/z and charge, assuming a proton is the charge donator
+        /// </summary>
+        /// <param name="mz">The given m/z</param>
+        /// <param name="charge">The given charge</param>
+        /// <returns>The mass</returns>
+        public static double MassFromMz(double mz, int charge)
+        {
+            return Math.Abs(charge) * mz - charge * Constants.PROTON;
+        }
+
+        /// <summary>
+        /// Calculates the m/z of a given mass and chargem assuming a proton is the charge donator
+        /// </summary>
+        /// <param name="mass">The given mass</param>
+        /// <param name="charge">The given charge</param>
+        /// <returns>The m/z</returns>
+        public static double MzFromMass(double mass, int charge)
+        {
+            return mass / Math.Abs(charge) + Math.Sign(charge) * Constants.PROTON;
+        }
+
+        #endregion Static Methods
+    }
+}
