ab68b1892b93e40b8d9c427043165394bfbe5690
Derek Bailey <dbaileychess@gmail.com>
2013-02-14 11:21:37 -0600

    Added new namespace CSMSL.Analysis to handle algorithms that pertain to proteomics datasets

---
+++ CSMSL/Analysis/Identification/Morpheus.cs
@@ -0,0 +1,43 @@
+﻿///////////////////////////////////////////////////////////////////////////
+//  Morpehus.cs - Peptide Spectral Identification Algorithm               /
+//                                                                        /
+//  Please cite the following publication when using these algorithms     /
+//  in a publication:                                                     /
+//                                                                        /
+//  Wenger CD, Coon JJ. "A Proteomics Search Algorithm Specifically       /
+//  Designed for High-Resolution Tandem Mass Spectra" Journal of Proteome /
+//  Research (in press)                                                   /
+//                                                                        /
+//  http://pubs.acs.org/doi/abs/10.1021/pr301024c                         /
+//                                                                        /
+//  Copyright 2013 Craig D. Wenger & Derek J. Bailey                      /
+//  This file is part of CSMSL.                                           /
+//                                                                        /
+//  CSMSL is free software: you can redistribute it and/or modify         /
+//  it under the terms of the GNU General Public License as published by  /
+//  the Free Software Foundation, either version 3 of the License, or     /
+//  (at your option) any later version.                                   /
+//                                                                        /
+//  CSMSL is distributed in the hope that it will be useful,              /
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of        /
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         /
+//  GNU General Public License for more details.                          /
+//                                                                        /
+//  You should have received a copy of the GNU General Public License     /
+//  along with CSMSL.  If not, see <http://www.gnu.org/licenses/>.        /
+///////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSMSL.Analysis.Identification
+{
+    /// <summary>
+    /// High-resolution Proteomic Search Algorithm
+    /// </summary>
+    public class Morpheus
+    {
+    }
+}
---
+++ CSMSL/Analysis/Quantitation/IsobaricTag.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSMSL.Analysis.Quantitation
+{
+    public class IsobaricTag
+    {
+    }
+}
