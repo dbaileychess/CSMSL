4748e12abaa5f742443b22092600a1f249a78273
Nicholas Kwiecien <kwiecien@wisc.edu>
2013-03-27 14:22:06 -0500

    Added example .mzML files for testing

--- CSMSL.Examples/Examples.cs
+++ CSMSL.Examples/Examples.cs
@@ -62,7 +62,7 @@ namespace CSMSL.Examples
             //TrypticDigestion.Start(Protease.Trypsin);
 
             //Example IO
-            //MsIOExamples();
+            MsDataFileExamples.Start();
 
             // Omssa Reader
             //OmssaReader.Start();
@@ -71,42 +71,7 @@ namespace CSMSL.Examples
             //MorpheusSearch.Start(Protease.Trypsin);
         }
 
-        private static void MsIOExamples()
-        {
-
-            Console.WriteLine("**MS I/O Examples**");
-            Console.WriteLine("{0,-4} {1,3} {2,-6:F4} {3,-5} {4,7} {5,-10} {6}", "SN", "Msn", "RT", "Polarity", "# Peaks", "Analyzer", "M/Z Range");         
-
-            List<MSDataFile> exampleRawFiles = new List<MSDataFile>();
-            exampleRawFiles.Add(new ThermoRawFile("Resources/ThermoRawFileMS1MS2.raw"));
-            //exampleRawFiles.Add(new AgilentDDirectory("Resources/AgilentDDirectoryMS1MS2.d"));   
-            //exampleRawFiles.Add(new BrukerDDirectory(@"E:\Software\Third Parties\BRUKER sample data & executable\BRUKER sample data & executable\10 fmol BSA_01_637.d"));
-            foreach(MSDataFile dataFile in exampleRawFiles)  
-            {
-                dataFile.Open();
-                Stopwatch watch = new Stopwatch();
-                watch.Start();
-                foreach (MSDataScan scan in dataFile)
-                {
-                    List<MZPeak> peaks = null;           
-                    scan.MassSpectrum.TryGetPeaks(500, 510, out peaks);
-                    Console.WriteLine(peaks.Count);
-                    Console.WriteLine("{0,-4} {1,3} {2,-6:F4} {3,-5} {4,7} {5,-10} {6}",
-                        scan.SpectrumNumber,
-                        scan.MsnOrder,
-                        scan.RetentionTime,
-                        scan.Polarity,
-                        scan.MassSpectrum.Count,
-                        scan.MzAnalyzer,                      
-                        scan.MzRange);     
-                }
-                watch.Stop();
-                Console.WriteLine("File: {0}", dataFile.Name);
-                Console.WriteLine("Time: {0}", watch.Elapsed);
-                Console.WriteLine("Memory used: {0:N0} MB", System.Environment.WorkingSet / (1024 * 1024));
-            }    
-        }
-
+   
         private static void VennDiagramExamples()
         {
             Console.WriteLine("**Venn Diagram Examples**");
