2099b182cd77c3a361bb0c451b55439dbb7971ff
Anna Merrill <anna.e.larson@gmail.com>
2013-04-08 10:34:17 -0500

    Added Quant Channel Set Tests

---
+++ CSMSL.Tests/Analysis/Quantitation/QuantitationChannelSetTestFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Should.Fluent;
+using CSMSL.Analysis.Quantitation;
+using CSMSL.Proteomics;
+using CSMSL.Chemistry;
+
+namespace CSMSL.Tests.Analysis.Quantitation
+{
+    [TestFixture]
+    [Category("Quantitation")]
+    public sealed class QuantitationChannelSetTestFixture
+    {
+        private QuantitationChannelSet _TMT6plex;
+        private QuantitationChannelSet _lysine6plex;
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+    }
+}
