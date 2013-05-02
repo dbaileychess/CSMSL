d8d59727eee3185ec58db5952de3d2dfb73ade59
Derek Bailey <dbaileychess@gmail.com>
2013-05-02 09:41:27 -0500

    Added basic fragment collection class

---
+++ CSMSL/Proteomics/FragmentCollection.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSMSL.Proteomics
+{
+    public class FragmentCollection : IEnumerable<Fragment>, ICollection<Fragment>
+    {
+        private AminoAcidPolymer _parent;
+
+        private List<Fragment> _fragments;
+
+        public int Count { get { return _fragments.Count; } }
+
+        public AminoAcidPolymer Parent { get { return _parent; } }
+
+        public int NTerminalFragments { get { return _fragments.Count(frag => frag.Type < FragmentTypes.x); } }
+
+        public int CTerminalFragments { get { return _fragments.Count(frag => frag.Type >= FragmentTypes.x); } }
+
+        public FragmentCollection(IEnumerable<Fragment> fragments, AminoAcidPolymer parent = null)
+        {
+            _fragments = new List<Fragment>(fragments);
+            _parent = parent;
+        }
+
+        public FragmentCollection(AminoAcidPolymer parent = null)
+        {
+            _fragments = new List<Fragment>();
+            _parent = parent;
+        }
+               
+        public FragmentCollection Filter(Func<Fragment, bool> predicate)
+        {
+            return new FragmentCollection(_fragments.Where(predicate));
+        }
+
+        public IEnumerator<Fragment> GetEnumerator()
+        {
+            return _fragments.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _fragments.GetEnumerator();
+        }
+
+        public void Add(Fragment item)
+        {
+            _fragments.Add(item);
+        }
+
+        public void Clear()
+        {
+            _fragments.Clear();
+        }
+
+        public bool Contains(Fragment item)
+        {
+            return _fragments.Contains(item);
+        }
+
+        public void CopyTo(Fragment[] array, int arrayIndex)
+        {
+            _fragments.CopyTo(array, arrayIndex);
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(Fragment item)
+        {
+            return _fragments.Remove(item);
+        }
+
+
+    }
+}
