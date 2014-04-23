fcb74a13827de27e7d6cc09803b3b15b3233e38f
Derek Bailey <dbaileychess@gmail.com>
2014-04-23 09:24:54 -0500

    Readded mzidentml

---
+++ CSMSL.Examples/MzIdentMLExamples.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSMSL.IO.MzIdentML;
+
+namespace CSMSL.Examples
+{
+    public class MzIdentMLExamples
+    {
+
+        public static void ReadMzIdentML()
+        {
+            //string filePath = "Resources/omssa_example.mzid";
+            string filePath = System.IO.Path.Combine(Examples.BASE_DIRECTORY, "example2.mzid");
+            MzidReader reader = new MzidReader(filePath);
+            reader.Open();
+
+            string version = reader.Version;
+
+            Console.WriteLine("Version: " + version);
+
+            //reader.Version = "1.1.1";
+
+            string outputPath = System.IO.Path.Combine(Examples.BASE_DIRECTORY, "example.mzid");
+
+            reader.SaveAs(outputPath);
+
+        }
+
+    }
+}
