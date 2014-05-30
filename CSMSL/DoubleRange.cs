1eddc51199a65aec62000855bfe64ca8b52b3a19
Derek Bailey <dbaileychess@gmail.com>
2014-05-30 10:45:46 -0500

    Minor updates

--- CSMSL/DoubleRange.cs
+++ CSMSL/DoubleRange.cs
@@ -13,6 +13,16 @@ namespace CSMSL
         public DoubleRange(IRange<double> range)
             : this(range.Minimum, range.Maximum) { }
 
+        /// <summary>
+        /// Creates a full-width range around some mean value
+        /// i.e. 10 ppm at 500 would give you 499.9975 - 500.0025
+        /// which has a width of 0.005. Coverting back to ppm 
+        /// (1e6) *0.005 / 500 = 10 ppm.
+        /// The differnce from the mean value to an boundary is exactly
+        /// half the tolearnce you specified
+        /// </summary>
+        /// <param name="mean"></param>
+        /// <param name="tolerance"></param>
         public DoubleRange(double mean, Tolerance tolerance)
         {
             SetTolerance(mean, tolerance);
@@ -61,6 +71,11 @@ namespace CSMSL
             {
                 return Maximum - Minimum;              
             }
+        }
+
+        public double ToPPM()
+        {
+            return 1e6 * Width / Mean;
         }
 
         public static DoubleRange FromPPM(double mean, double ppmTolerance)
